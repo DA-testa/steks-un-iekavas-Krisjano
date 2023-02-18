bc7a282d22ae8f7169181f1476f918d80005d832
Krisjanis Adamsons 221REC031 <112925675+Krisjano@users.noreply.github.com>
2023-02-18 10:28:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,20 +14,21 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next_char, i+1))
-          elif next_char in ")]}":
+        elif next_char in ")]}":
                 if not opening_brackets_stack:
                     return i+1
                 pedejais = opening_bracket_stack.pop()
                 if not are_matching(pedejais.char, next.char):
                     return i +1
-             if opening_brackets_stack:
-                retrun opening_brackets_stack[0].position
-            else : retrun "Success"
+        if opening_brackets_stack:
+                return opening_brackets_stack[0].position
+        else : 
+                 return "Success"
           
 def main():
     text = input()
     mismatch = find_mismatch(text)
-   print(mismatch)
+    print(mismatch)
 
 
 if __name__ == "__main__":
