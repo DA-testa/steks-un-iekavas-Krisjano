eafec45aefac5c34e36f1d4127281a6cf39c1d52
Krisjanis Adamsons 221REC031 <112925675+Krisjano@users.noreply.github.com>
2023-02-18 11:08:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            opening_brackets_stack.append((next,i))
+    for i, next_char in enumerate(text):
+        if next_char in "([{":
+            opening_brackets_stack.append((next_char,i))
       
-        if next in ")]}":
+        elif next_char in ")]}":
             if not opening_brackets_stack:
                 return i +1
             pedejais = opening_brackets_stack.pop()
             
-            if not are_matching(pedejais[0], next):
+            if not are_matching(pedejais.char, next_char):
                 return i+1
                 
             if opening_brackets_stack:
-              return opening_brackets_stack[0][1] + 1
+              return opening_brackets_stack[0].position + 1
     
         return "Success"
 
